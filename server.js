0675368f5ce5f821630718882ea62a82c87519d7
burak <burakkisacik@yahoo.com>
2021-03-15 21:00:11 +0300

    routes created

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ dotenv.config({ path: './config/config.env' });
 //initialize app
 const app = express();
 
+//create routes
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show single bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 //define the port
 const PORT = process.env.PORT || 5000;
 
