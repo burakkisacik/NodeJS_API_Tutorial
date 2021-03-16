992f55ee4dd1292c8d1e9f555e7e8eeeb25633c3
burak <burakkisacik@yahoo.com>
2021-03-16 13:49:30 +0300

    Model created: using mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const colors = require('colors');
 
 //load env file in order to use environment variables
 dotenv.config({ path: './config/config.env' });
@@ -30,12 +31,13 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
 
 // handle the unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //Close server & exit process
   server.close(() => process.exit(1));
 });
