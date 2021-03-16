745c7926335aa0ea28e7c4439f8f305cc9f94fd9
burak <burakkisacik@yahoo.com>
2021-03-16 21:24:32 +0300

    errorhandler done: this part is important to understand ATTENTION HEREgit add .!

--- middleware/error.js
+++ middleware/error.js
@@ -7,9 +7,22 @@ const errorHandler = (err, req, res, next) => {
   //Log to console for developer
   console.log(err.stack.red);
 
+  //Mongoose bad ObjectId Error
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose duplicate key Error
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose Validation Error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
