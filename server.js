b997aaf1965e182c07b730578799eede2c126e4e
burak <burakkisacik@yahoo.com>
2021-04-01 17:08:28 +0300

    Prevent NoSQL injections & Sanitize Data

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const errorHandler = require('./middleware/error');
 const fileupload = require('express-fileupload');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 
 //load env file in order to use environment variables
 dotenv.config({ path: './config/config.env' });
@@ -37,6 +38,9 @@ if (process.env.NODE_ENV === 'development') {
 
 //File uploading
 app.use(fileupload());
+
+//Sanitize data
+app.use(mongoSanitize());
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
